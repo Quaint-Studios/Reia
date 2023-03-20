af7f7b8338d22febcd00dc2e83a594254a953929
Makosai <kristopher.ali.dev@gmail.com>
2023-03-20 10:13:34 -0400

    Create Multi Build Manager.
    Former-commit-id: 7325121b2511da5a235d2aa3081fc9863252e582

---
+++ Assets/Thirdparty/com.teamon.buildmanager/Editor/BuildManager/BuildPipeline/BuildSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[Serializable]
+public class BuildSequence : ICloneable {
+	public bool isEnabled;
+
+	public string editorName;
+
+	public string scriptingDefineSymbolsOverride;
+
+	public List<BuildData> builds = new List<BuildData>() { new BuildData() };
+
+	public BuildSequence() : this("New build sequence", new BuildData()) {
+	}
+
+	public BuildSequence(string editorName, params BuildData[] builds) {
+		this.editorName = editorName;
+		this.builds = new List<BuildData>(builds);
+
+		scriptingDefineSymbolsOverride = "";
+		
+		isEnabled = true;
+	}
+
+	public object Clone() {
+		BuildSequence sequence = this.MemberwiseClone() as BuildSequence;
+
+		sequence.builds = new List<BuildData>(builds.Count);
+		for (int i = 0; i < builds.Count; ++i) {
+			sequence.builds.Add(builds[i].Clone() as BuildData);
+		}
+
+		return sequence;
+	}
+}
