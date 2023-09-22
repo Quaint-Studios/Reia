7bb0f870e5a174909d851dc0c60f059343b367f4
Makosai <kristopher.ali.dev@gmail.com>
2023-09-22 05:01:46 -0400

    Add an early combat test and physics layers.
    Former-commit-id: 5c242b216ea40ebd35a8f5c4a38ef0d739eb3845

--- scripts/mob/attackable/player/player.gd
+++ scripts/mob/attackable/player/player.gd
@@ -13,6 +13,10 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _process(_delta):
+	if position.y <= -10: # handle falling off the map for now
+		position.y = 1
+
 func _input(_event: InputEvent):
 	if Input.is_action_just_pressed("quit"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -23,10 +27,6 @@ func _input(_event: InputEvent):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		print("click")
-
-	if Input.is_action_just_pressed("attack"):
-		GameUI.instance.status_bars.set_health(72, 100)
-		print("attack")
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -63,6 +63,8 @@ func handle_camera(event: InputEvent):
 	camera.position.z =  pos_z_formula
 
 func handle_inputs():
+	handle_attack()
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -70,7 +72,7 @@ func handle_inputs():
 	# Get the input direction and handle movement.
 
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	print("input dir: ", input_dir)
+	if(input_dir): print("input dir: ", input_dir)
 	var direction = (camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -82,3 +84,18 @@ func handle_inputs():
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+func handle_attack():
+	if Input.is_action_just_pressed("attack"):
+		# GameUI.instance.status_bars.set_health(72, 100)
+		var space = get_world_3d().direct_space_state
+		var query = PhysicsRayQueryParameters3D.create(camera.global_position,
+			camera.global_position - camera.global_transform.basis.z * 100, PhysicsUtils.arr_to_collision_mask(
+				[ PhysicsUtils.ENEMY_MASK ]
+			))
+		var collision = space.intersect_ray(query)
+		if collision:
+			print("Attacked:", collision.collider.name)
+			_attack(collision.collider)
+		else:
+			print("Collide with:", "nuthin")
