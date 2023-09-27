b0b81abf7d52b95e70a102fbfcb851c0823e8012
Makosai <kristopher.ali.dev@gmail.com>
2023-09-27 04:50:48 -0400

    Add functionality to the Game Manager.
    Add a way to set the FPS with certain conditions (if the player is paused or viewing a UI). And add a way to setup the sound manager. Also adds a Constants script.
    
    
    Former-commit-id: 7572dda669d1fbfbb894f738d4b436d61677e0d1

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -1,7 +1,26 @@
-extends Node
+class_name GameManager extends Node
 
-@onready var my_player = get_node("../../Players/MyPlayer")
+static var is_paused = false
+static var is_in_ui = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	GameManager.update_fps()
+	GameManager.setup_sound_manager()
+
+static func toggle_pause():
+	is_paused = !is_paused
+	update_fps()
+
+static func toggle_is_in_ui():
+	is_in_ui = !is_in_ui
+	update_fps()
+
+static func update_fps():
+	var max_fps = Constants.UI.DEFAULT_FPS if is_paused or is_in_ui else Constants.Game.DEFAULT_FPS
+	Engine.set_max_fps(max_fps)
+
+static func setup_sound_manager():
+	SoundManager.set_default_music_bus("Music")
+	SoundManager.set_default_sound_bus("SFX")
+	SoundManager.set_default_ui_sound_bus("Interface")
+	SoundManager.set_default_dialogue_bus("Dialogue")
