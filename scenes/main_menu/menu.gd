44108c333facb4733656b3a54eae371322a1220d
Makosai <kristopher.ali.dev@gmail.com>
2023-09-27 04:46:37 -0400

    Improve the main menu ui.
    Added the temp logo, volume page, and some headers.
    
    
    Former-commit-id: 04aecfe2e387acd33d7eccab6d5ecda2805f3587

--- scenes/main_menu/menu.gd
+++ scenes/main_menu/menu.gd
@@ -1,22 +1,32 @@
 extends Control
 
-@onready var MainRoot = $Margins/Main
-@onready var SettingsRoot = $Margins/Settings
+@export var menu_music: AudioStream
 
 @onready var packed_scene_reia = preload(SceneSelector.REIA)
 
-func _ready() -> void:
-	Engine.set_max_fps(60)
+func _ready():
+	_prepare_sound()
+	pass
+
+func _prepare_sound():
+	SoundManager.set_sound_volume(0.5)
+	#%SoundVolume.text = "50%"
+	SoundManager.set_music_volume(0.5)
+	#%MusicVolume.text = "50%"
+
+	SoundManager.play_music(menu_music, 1.0)
 
 ###
 ### Main Functions
 ###
 func _on_play_pressed():
+	if GameManager.is_in_ui:
+		GameManager.toggle_is_in_ui()
 	get_tree().change_scene_to_file(SceneSelector.REIA)
 
 func _on_settings_pressed():
-	MainRoot.visible = false
-	SettingsRoot.visible = true
+	%Main.visible = false
+	%Settings.visible = true
 
 func _on_exit_pressed():
 	get_tree().quit()
@@ -25,11 +35,43 @@ func _on_exit_pressed():
 ### Settings
 ###
 func _on_controls_pressed():
-	pass # Replace with function body.
+	disable_all_roots()
+	%Controls.visible = true
 
 func _on_volume_pressed():
-	pass # Replace with function body.
+	disable_all_roots()
+	%Volume.visible = true
 
 func _on_back_pressed():
-	MainRoot.visible = true
-	SettingsRoot.visible = false
+	if %Settings.visible:
+		disable_all_roots()
+		%Main.visible = true
+
+	if %Controls.visible || %Volume.visible:
+		disable_all_roots()
+		%Settings.visible = true
+
+func disable_all_roots():
+	%Main.visible = false
+	%Settings.visible = false
+	%Controls.visible = false
+	%Volume.visible = false
+
+
+func _on_master_volume_changed(value):
+	pass # Replace with function body.
+
+
+func _on_music_volume_changed(value):
+	SoundManager.set_music_volume(linear_to_db(value))
+	pass # Replace with function body.
+
+func _on_sfx_volume_changed(value):
+	SoundManager.set_sound_volume(linear_to_db(value))
+	pass # Replace with function body.
+
+func _on_ui_volume_changed() -> void:
+	pass # Replace with function body.
+
+func _on_dialogue_volume_changed(value):
+	pass # Replace with function body.
