7a9cad5d901ccd545241e38f3c752b022948cfc2
Makosai <kristopher.ali.dev@gmail.com>
2023-09-28 16:08:22 -0400

    Update the Game Manager to use the new Constants and enums.

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -1,22 +1,37 @@
 class_name GameManager extends Node
 
-static var is_paused = false
-static var is_in_ui = true
+static var current_ui: UI_TYPES = UI_TYPES.MAIN_MENU
+enum UI_TYPES { PLAY, PAUSE, MAIN_MENU, INVENTORY }
 
 func _ready():
 	GameManager.update_fps()
 	GameManager.setup_sound_manager()
 
-static func toggle_pause():
-	is_paused = !is_paused
-	update_fps()
+func _input(_event: InputEvent):
+	# Let the UI handle the rest
+	if current_ui != UI_TYPES.PLAY:
+		return
 
-static func toggle_is_in_ui():
-	is_in_ui = !is_in_ui
+	if Input.is_action_just_pressed("quit"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		print("quit")
+
+	if Input.is_action_just_pressed("click"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		print("click")
+
+static func toggle_pause():
+	match current_ui:
+		UI_TYPES.PLAY:
+			current_ui = UI_TYPES.PAUSE
+		UI_TYPES.PAUSE:
+			current_ui = UI_TYPES.PLAY
 	update_fps()
 
 static func update_fps():
-	var max_fps = Constants.UI.DEFAULT_FPS if is_paused or is_in_ui else Constants.Game.DEFAULT_FPS
+	var max_fps = Constants.UI_DEFAULT_FPS if current_ui == UI_TYPES.PLAY else Constants.GAME_DEFAULT_FPS
 	Engine.set_max_fps(max_fps)
 
 static func setup_sound_manager():
@@ -24,3 +39,4 @@ static func setup_sound_manager():
 	SoundManager.set_default_sound_bus("SFX")
 	SoundManager.set_default_ui_sound_bus("Interface")
 	SoundManager.set_default_dialogue_bus("Dialogue")
+
