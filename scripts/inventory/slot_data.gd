2be8e58e8597bf39ddbb2d3fdd30614ecf2219db
Makosai <kristopher.ali.dev@gmail.com>
2023-09-28 16:11:10 -0400

    Start implementing the Inventory system.
    Former-commit-id: 47184900fae0190ffd5d965054971358ef8b4324

---
+++ scripts/inventory/slot_data.gd
@@ -0,0 +1,4 @@
+class_name SlotData extends Resource
+
+@export var item_data: ItemData
+@export var quantity: int = 1
---
+++ scripts/items/item_data.gd
@@ -0,0 +1,7 @@
+class_name ItemData extends Resource
+
+@export var unique_id: int
+@export var name: String = ""
+@export_multiline var description: String = ""
+@export var stackable: bool = false
+@export var texture: AtlasTexture
