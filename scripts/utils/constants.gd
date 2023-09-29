4e971847b52a835ce9287a5b65694542e8786c47
Makosai <kristopher.ali.dev@gmail.com>
2023-09-29 05:04:22 -0400

    Update the INVENTORY_SELECTOR_POS for the UI.

--- scripts/utils/constants.gd
+++ scripts/utils/constants.gd
@@ -7,7 +7,7 @@ const INVENTORY_BG_COLOR: String = "07263d"
 const INVENTORY_FG_COLOR: String = "ffffff"
 const INVENTORY_MIN_BG_BLUR: float = 0.0
 const INVENTORY_MAX_BG_BLUR: float = 3.0
-static func INVENTORY_SELECTOR_POS(i: int) -> float: return 81.25*(i*2)-81.25 # Needs an offset to the left, partially works right now
+static func INVENTORY_SELECTOR_POS(i: int) -> float: return -(81.25*((i-2)*2)-81.25) # Needs an offset to the left, partially works right now
 
 const UI_DEFAULT_FPS = 30
 const GAME_DEFAULT_FPS = 60
