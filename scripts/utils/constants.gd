133e188da6624e523a4ab7fc45cdcaef9b01bc91
Makosai <kristopher.ali.dev@gmail.com>
2023-10-02 16:14:54 -0400

    Update the inventory constant colors to include alphas.
    Former-commit-id: 4689139d895875316029145e1a35478337407ee1

--- scripts/utils/constants.gd
+++ scripts/utils/constants.gd
@@ -3,8 +3,8 @@ class_name Constants
 const DEFAULT_ATLAS_SIZE = 128
 
 const INVENTORY_ATLAS_SIZE: int = 128
-const INVENTORY_BG_COLOR: String = "07263d"
-const INVENTORY_FG_COLOR: String = "ffffff"
+const INVENTORY_BG_COLOR: String = "07263dc8"
+const INVENTORY_FG_COLOR: String = "ffffffff"
 const INVENTORY_MIN_BG_BLUR: float = 0.0
 const INVENTORY_MAX_BG_BLUR: float = 3.0
 static func INVENTORY_SELECTOR_POS(i: int) -> float: return -(81.25*((i-2)*2)-81.25) # Needs an offset to the left, partially works right now
