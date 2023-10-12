dc2000cbea49cf386e969173a1870d948dcbb95f
Makosai <kristopher.ali.dev@gmail.com>
2023-10-12 11:47:21 -0400

    Improve the water shader.
    Former-commit-id: ac5bc10d4cdf0cc6f5dd5f739fee9ad1e39c51d1

--- scenes/shader_test/shaders/water_helper.gd
+++ scenes/shader_test/shaders/water_helper.gd
@@ -3,11 +3,11 @@ extends Node3D
 @export var testing := true
 
 func _ready():
-	var cr = $Simulation/ColorRect
-	var crb = $SimulationBuffer/ColorRect
+	var _cr = $Simulation/ColorRect
+	var _crb = $SimulationBuffer/ColorRect
 	var water = $Water
 	var sim_tex = $Simulation.get_texture()
-	var col_tex = $Collision.get_texture()
+	var _col_tex = $Collision.get_texture()
 
 	water.mesh.surface_get_material(0).set_shader_parameter('simulation', sim_tex) # fragment
 	water.mesh.surface_get_material(0).set_shader_parameter('v_simulation', sim_tex) # vertex
@@ -15,9 +15,3 @@ func _ready():
 	if testing:
 		print("Testing the water shader. You can disable this variable later.")
 		get_node("../Tests/Ball/AnimationPlayer").play("bounce")
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
