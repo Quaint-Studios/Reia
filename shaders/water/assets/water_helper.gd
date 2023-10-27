71e8d61a3145be13b856ed5576698272b927408d
Makosai <kristopher.ali.dev@gmail.com>
2023-10-27 12:22:37 -0400

    Improve the water helper.
    Former-commit-id: a1e3aaebbf2e1b433cd801c267657cbae9d96d95

--- shaders/water/assets/water_helper.gd
+++ shaders/water/assets/water_helper.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
-@export var testing := true
+@export var testing := false
+@export var size = Vector2(14, 14);
+@export var detail = Vector2(50, 50);
 
 func _ready():
 	var _cr = $Simulation/ColorRect
 	var _crb = $SimulationBuffer/ColorRect
-	var water = $Water
+	var water: MeshInstance3D = $Water
+	var water_mesh: PlaneMesh = water.mesh as PlaneMesh;
+	water_mesh.size = size;
+	water_mesh.subdivide_width = detail.x;
+	water_mesh.subdivide_depth = detail.y;
+
 	var sim_tex = $Simulation.get_texture()
 	var _col_tex = $Collision.get_texture()
 
