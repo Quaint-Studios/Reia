84becabb3f56b52202f171326cdb0dcb0a106247
Makosai <kristopher.ali.dev@gmail.com>
2023-10-29 06:26:13 -0400

    Create scripts for possible ItemData types and create a Soulstone Slot for WeaponData.

---
+++ scripts/items/data/soulstone_slot.gd
@@ -0,0 +1,7 @@
+class_name SoulstoneSlot extends Resource
+
+@export var slot_type : Enums.Soulstone.Type
+@export var slot_affinity : Enums.Element.Affinity
+
+func add_soulstone(soulstone: SoulstoneData):
+	pass
