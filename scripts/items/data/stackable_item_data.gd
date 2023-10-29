ec6a09151ff57e80fd6100a0954574656b174c63
Makosai <kristopher.ali.dev@gmail.com>
2023-10-29 07:48:43 -0400

    Remove unused argument from decrement_item().
    Former-commit-id: 1cf625c60d78cf221fbc6ea49d7201f28a53c4fc

--- scripts/items/data/stackable_item_data.gd
+++ scripts/items/data/stackable_item_data.gd
@@ -6,6 +6,6 @@ func increment_item(_quantity:= 1):
 	quantity += quantity
 	return self
 
-func decrement_item(item_name: String, _quantity:= -1):
+func decrement_item(_quantity:= -1):
 	increment_item(_quantity) # Hehe lazy
 	return self
