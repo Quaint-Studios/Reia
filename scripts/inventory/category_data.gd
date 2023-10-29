97b6a5f43646187765f77459e860e662213eb5c5
Makosai <kristopher.ali.dev@gmail.com>
2023-10-29 07:47:31 -0400

    Improve error handling in CategoryData along with a fix.
    Former-commit-id: 7410ede6f91ec8497e2b0a858b592edec7543a5f

--- scripts/inventory/category_data.gd
+++ scripts/inventory/category_data.gd
@@ -8,20 +8,22 @@ func add_item(item: ItemData):
 	items[item.name] = item
 	return self
 
-func get_item(item_name: String):
+func get_item(item_name: String) -> ItemData:
 	if !items.has(item_name):
-		return false # Item doesn't exist
+		print_debug("Item %s in category %s is null | Stack: %s" % [item_name, name, get_stack()])
+		return # Item doesn't exist
 
 	return items[item_name]
 
 func increment_item(item_name: String, quantity:= 1):
 	var item = get_item(item_name)
 
-	if item == false:
-		return false
+	if item == null:
+		return self
 
-	if !is_instance_of(item, StackableItemData):
-		return false # Item isn't stackable
+	if !item is StackableItemData:
+		print_debug("Item %s in category %s is not a StackableObject | Stack: %s" % [item_name, name, get_stack()])
+		return self # Item isn't stackable
 
 	(item as StackableItemData).increment_item(quantity)
 	return self
