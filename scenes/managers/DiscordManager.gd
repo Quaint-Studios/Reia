245b82bfc72daccd990ce72dbfd85b3de0b2b57d
Makosai <kristopher.ali.dev@gmail.com>
2023-10-30 10:37:13 -0400

    Create a manager for Discord (Rich Presence support)
    Former-commit-id: 5a94b494c02c0b3a2730627ae50c81efdc2d8290

---
+++ scenes/managers/DiscordManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func _ready():
+	discord_sdk.app_id = 688581385639952418 # Application ID
+	discord_sdk.details = "Roaming the world..."
+	discord_sdk.state = "Playing Solo"
+
+	discord_sdk.large_image = "logo_reia" # Image key from "Art Assets"
+	discord_sdk.large_image_text = ""
+	discord_sdk.small_image = "" # Image key from "Art Assets"
+	discord_sdk.small_image_text = ""
+
+	discord_sdk.current_party_size = 1
+	discord_sdk.max_party_size = 5
+
+	discord_sdk.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+	# discord_sdk.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
+
+	discord_sdk.refresh() # Always refresh after changing the values!
