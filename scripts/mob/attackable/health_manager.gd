5b2b37187ee5d189fd77088d9be999b3c2121835
Makosai <kristopher.ali.dev@gmail.com>
2023-11-01 18:35:58 -0400

    Update the Attackables to have a proper signal with HealthManager.
    Former-commit-id: 569f7d5d07174dbd6b3541e86d5072b48cae4045

--- scripts/mob/attackable/health_manager.gd
+++ scripts/mob/attackable/health_manager.gd
@@ -1,6 +1,6 @@
 class_name HealthManager extends Node3D
 
-@export var attackable_stats: AttackableStats
+@export var attackable: Attackable
 
 @onready var health_bar: TextureRect = $SubViewport/HealthBar/SVC/SV/Bar
 @onready var health_value: RichTextLabel = $SubViewport/HealthBar/Value
@@ -10,13 +10,14 @@ const health_size = Vector2(1246, 177)
 const TWEEN_DURATION = 0.12345
 
 func _ready():
-	if attackable_stats == null:
+	if attackable == null:
 		print_debug("HealthManager's attackable_stats is null | Stack: %s" % get_stack())
 		return
-		attackable_stats.health_changed.connect(set_health)
 
-func set_health(current_health, max_health):
-	var pos_x = health_start_x - (health_size.x - health_size.x * (float(current_health) / float(max_health)))
+	attackable.stats.health_changed.connect(_set_health)
+
+func _set_health(health: int, max_health: int):
+	var pos_x = health_start_x - (health_size.x - health_size.x * (float(health) / float(max_health)))
 	var tween = get_tree().create_tween()
 	tween.tween_property(health_bar, "position", Vector2(pos_x, health_bar.position.y), TWEEN_DURATION)
-	health_value.text = "[center]{c}/{m}".format({ "c": current_health, "m": max_health })
+	health_value.text = "[center]%s/%s" % [health, max_health]
