31b10760b24e532d128d374a017f9f6e6bf385c8
Makosai <kristopher.ali.dev@gmail.com>
2023-11-02 22:38:00 -0400

    Improve the combat system.

--- src/combat/weapon_stats.gd
+++ src/combat/weapon_stats.gd
@@ -1,11 +1,6 @@
-extends Node
+class_name WeaponStats extends BaseStats
 
+@export var weapon_type := WeaponType.Melee
+enum WeaponType { Melee, Bow, Spell }
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var damage_type: Enums.Element.Affinity
--- src/item/item.gd
+++ src/item/item.gd
@@ -1,6 +1,6 @@
-class_name ItemData extends Resource
+class_name Item extends Resource
 
-@export var unique_id: int
+@export var id: int
 @export var name: String = ""
 @export_multiline var description: String = ""
 @export var texture: AtlasTexture
