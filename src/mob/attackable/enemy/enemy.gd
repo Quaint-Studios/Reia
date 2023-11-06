732a9186c314e46d6e63034a35feb1209da0850d
Makosai <kristopher.ali.dev@gmail.com>
2023-11-06 12:59:24 -0500

    Create a LootSpawner.
    Former-commit-id: c2a4c1031cc1172e878f3d2097b396a91bc4d120

--- src/mob/attackable/enemy/enemy.gd
+++ src/mob/attackable/enemy/enemy.gd
@@ -1,15 +1,24 @@
 class_name Enemy extends Attackable
 
 # Add things like pathfinding and AI
-var loot: Array[Item] = []
+var loot: Array[Item] = [] # TODO: Instead of Array[Item], use a Resource of type LootTable
+#  LootTable should have the items: Array[Item] in it. This way it can be used
+# in things like Bestiaries to get the loot the enemy drops.
 
 func _init():
 	status.state_changed.connect(_on_died)
 
 func _on_died():
-	pass
 	# Spawn Item
-	# var dropped_items := DroppedItems.new(loot)
+	var loot_spawner := LootSpawner.instance
+
+	if loot_spawner == null:
+		print_debug("Error in Enemy %s, LootSpawner does not exist." % name)
+		# Consider generating a new LootSpawner... maybe LootSpawner.create()?
+		# Or Map.setup() so all maps always have the proper root setups?
+		return
+
+	loot_spawner.spawn(loot, position)
 
 	# attach a new timer.
 	# dissolve and delete this enemy.
