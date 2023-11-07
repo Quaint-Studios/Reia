25d12e44ef590f3ae34035d9113965a575bb204b
Makosai <kristopher.ali.dev@gmail.com>
2023-11-06 21:43:59 -0500

    Implement despawning for enemies.

--- src/mob/attackable/enemy/enemy.gd
+++ src/mob/attackable/enemy/enemy.gd
@@ -3,12 +3,15 @@ class_name Enemy extends Attackable
 # Add things like pathfinding and AI
 var loot: Array[Item] = [] # TODO: Instead of Array[Item], use a Resource of type LootTable
 #  LootTable should have the items: Array[Item] in it. This way it can be used
-# in things like Bestiaries to get the loot the enemy drops.
+# in things like Bestiaries to get the loot the enemy drops specifically.
 
-func _init():
+func _ready():
 	status.state_changed.connect(_on_died)
 
 func _on_died():
+	if status.state != AttackableStatus.Status.DEAD:
+		return
+
 	# Spawn Item
 	var loot_spawner := LootSpawner.instance
 
@@ -20,6 +23,15 @@ func _on_died():
 
 	loot_spawner.spawn(loot, position)
 
+	despawn()
+
 	# attach a new timer.
 	# dissolve and delete this enemy.
 	# create an enemy handler to reinstantiate this enemy.
+
+func despawn():
+	var timer := Timer.new()
+	timer.wait_time = 0.5
+	timer.timeout.connect(queue_free)
+	timer.autostart = true
+	add_child(timer)
