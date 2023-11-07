4014a65e6a0d98c75a5181c396a8e8e24f9da2e8
Makosai <kristopher.ali.dev@gmail.com>
2023-11-07 00:07:34 -0500

    Improve the dropped loot system.
    Former-commit-id: 4a005663409e08c17d0b72fc20b18481d398cb83

--- src/combat/attackable_status.gd
+++ src/combat/attackable_status.gd
@@ -1,17 +1,25 @@
 class_name AttackableStatus extends Resource
 
-signal state_changed(state: Status)
+signal is_alive
+signal is_dead
+signal is_respawning
 
 @export var state := Status.ALIVE:
 	set(value):
 		state = value
-		state_changed.emit(value)
+		match value:
+			Status.ALIVE:
+				is_alive.emit()
+			Status.DEAD:
+				is_dead.emit()
+			Status.RESPAWNING:
+				is_respawning.emit()
 enum Status { ALIVE, DEAD, RESPAWNING }
 
 func revive():
 	state = Status.ALIVE
 
-func die(attacker: Attackable):
+func die(_attacker: Attackable):
 	state = Status.DEAD
 
 func respawn():
