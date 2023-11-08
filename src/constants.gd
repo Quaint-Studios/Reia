cb5cd3dcb1ef8795e66b24fca2c15fe6fcf349d2
Makosai <kristopher.ali.dev@gmail.com>
2023-11-08 02:07:01 -0500

    Create constant colors for item grades.

--- src/constants.gd
+++ src/constants.gd
@@ -2,6 +2,13 @@ class_name Constants
 
 const DEFAULT_ATLAS_SIZE = 128
 
+# Items (May change)
+const ITEM_GRADE_COMMON_COLOR = "d2d2d2ff"
+const ITEM_GRADE_UNCOMMON_COLOR = "22d1ffff"
+const ITEM_GRADE_RARE_COLOR = "7200f0ff"
+const ITEM_GRADE_ARCANE_COLOR = "fdfd1cff"
+
+# Inventory
 const INVENTORY_ATLAS_SIZE: int = 128
 const INVENTORY_BG_COLOR: String = "07263dc8"
 const INVENTORY_FG_COLOR: String = "ffffffff"
@@ -9,5 +16,6 @@ const INVENTORY_MIN_BG_BLUR: float = 0.0
 const INVENTORY_MAX_BG_BLUR: float = 3.0
 static func INVENTORY_SELECTOR_POS(i: int) -> float: return -(81.25*((i-2)*2)-81.25) # Needs an offset to the left, partially works right now
 
+# Game
 const UI_DEFAULT_FPS = 30
 const GAME_DEFAULT_FPS = 60
