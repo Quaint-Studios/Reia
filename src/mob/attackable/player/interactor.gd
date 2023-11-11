71d8a33dcba8608dda06ad1efd3c36cfbbec8b39
Makosai <kristopher.ali.dev@gmail.com>
2023-11-10 19:55:19 -0500

    Further implement the Interactable script.
    Former-commit-id: 3102c77d1a318c7e0248e56d9a557930831e019f

--- src/mob/attackable/player/interactor.gd
+++ src/mob/attackable/player/interactor.gd
@@ -1,8 +1,8 @@
-extends RayCast3D
+class_name Interactor extends RayCast3D
 
 func _process(delta: float) -> void:
-	var coll := get_collider()
+	var coll := get_collider() as Interactable
 
 	if is_colliding():
 		if coll.is_in_group("interactable"):
-			UIManager.show_interaction("")
+			UIManager.show_interaction(coll.name)
