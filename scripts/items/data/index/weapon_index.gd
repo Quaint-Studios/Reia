49527ed5f41ec854b63d74170bf3544d48be1764
Makosai <kristopher.ali.dev@gmail.com>
2023-12-03 22:45:48 -0500

    Update the WeaponIndex to include the new items.
    Former-commit-id: 0bc2128d42b61fbb5cc07d06c1630206bbb82935

--- scripts/items/data/index/weapon_index.gd
+++ scripts/items/data/index/weapon_index.gd
@@ -1,3 +1,13 @@
 class_name WeaponIndex
 
 const WOODEN_SWORD = "res://scripts/items/weapons/1000_wooden_sword.tres" # 1000
+
+const SAPPHIRITE_WHIP = "res://rsc/items/weapons/3000_sapphirite_whip.tres" # 3000
+
+const PYROMANCERS_WAND = "res://rsc/items/weapons/5000_pyromancers_wand.tres" # 5000
+
+const ROBINS_BOW = "res://rsc/items/weapons/7000_robins_bow.tres" # 7000
+
+const ETHEREAL_BOW = "res://rsc/items/weapons/9000_ethereal_bow.tres" # 9000
+
+const STARLIT_SWORD = "res://rsc/items/weapons/10000_starlit_sword.tres" # 10000
