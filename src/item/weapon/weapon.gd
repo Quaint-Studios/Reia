464234c2440e3c40c6a763bcedbda72c5d82ef02
Makosai <kristopher.ali.dev@gmail.com>
2023-12-05 17:54:21 -0500

    Add level to the initializer for weapons.

--- src/item/weapon/weapon.gd
+++ src/item/weapon/weapon.gd
@@ -7,8 +7,9 @@ class_name Weapon extends Item
 var level := 1 # Caps out at 9, the 10th level is for upgrading the tier
 var experience := 0.0
 
-func _init(_experience := 0.0):
+func _init(_experience := 0.0, _level := 1):
 	item_type = ItemType.WEAPON
+	level = _level
 	experience = _experience
 
 func level_up():
@@ -18,8 +19,10 @@ func level_up():
 func xp_to_level(current_level: int, current_xp: float):
 	const multiplier = 100
 	# log(100^x)^x*(x*100)
-	# simplify function
-	var required_xp = pow(log(pow(multiplier, current_level)), current_level) * (current_level * multiplier)
+	# TODO: simplify function
+	var required_xp = pow(
+		log(pow(multiplier, current_level)), current_level
+	) * (current_level * multiplier)
 	if current_xp >= required_xp:
 		xp_to_level(current_xp - required_xp, current_level + 1)
 	else:
