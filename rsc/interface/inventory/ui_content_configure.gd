46da5a57fd710b2abaa5aeceb6013d9104d3dd7b
Makosai <kristopher.ali.dev@gmail.com>
2023-12-09 01:32:48 -0500

    Start creating a content section for upgrading and configuring items as well as craftables like potions and food.
    This new script will be versatile. It'll have functionality for all types but the functionality will utilize other scripts to make it modular.

---
+++ rsc/interface/inventory/ui_content_configure.gd
@@ -0,0 +1,15 @@
+@tool extends UI_ContentType
+###
+###  ui_content_configure.gdscript
+### 	This content type is opened by selecting any sort of enhancable or craftable item.
+### 	Weapons and Equipment can be enhanced and given soulstones.
+### 	Soulstones can be upgraded and can have their set effects evolve by mixing two soulstones
+### 	 several times. This will cause the primary soulstone to have its name changed to the new
+### 	 soulstone that is coming up with a prefix of "Unawakened"
+### 	Consumables can be used to craft better consumables. Herbs and such can be eaten raw or
+### 	 mixed together.
+### 	Quest Items can be inspected to give clues to the quest.
+
+
+func load_item(item: Item):
+	pass
