b0c577da3f333ac657fe8365f61d67c299f9d8f4
Makosai <kristopher.ali.dev@gmail.com>
2023-12-14 18:58:32 -0500

    Update the SceneSelector to also contain a Maps enum.
    Former-commit-id: facc240b400aafaa1f6b61bcb5b21c32123c4db1

--- scenes/scene_selector.gd
+++ scenes/scene_selector.gd
@@ -3,3 +3,5 @@ class_name SceneSelector extends Node
 const SCENE_FOLDER = "res://scenes/"
 const MAIN_MENU = SCENE_FOLDER + "/main_menu/main_menu.tscn"
 const REIA = SCENE_FOLDER + "reia/reia.tscn"
+
+enum Maps { REIA }
