a6e819f7126bd0c7aaddd52a65f72afa732bf760
Makosai <kristopher.ali.dev@gmail.com>
2023-12-14 22:50:47 -0500

    Get rid of a warning in ui_item.

--- rsc/interface/inventory/ui_item.gd
+++ rsc/interface/inventory/ui_item.gd
@@ -67,7 +67,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
