63c37afdef73361b0fae35be3abbc1b25d159d86
Makosai <kristopher.ali.dev@gmail.com>
2023-12-20 14:40:11 -0500

    Create the skeleton for the ServerManager.
    Former-commit-id: 590d933677d718843069fc1dcdcc3650669aa0d2

---
+++ src/managers/multiplayer/server_manager.gd
@@ -0,0 +1,34 @@
+class_name ServerManager extends Node
+## Creates a server and handles how many people can join it.
+##
+## The ServerManager is a part of the Multiplayer namespace.
+## It is only ran by authoritative devices and not clients.
+## Except when a consumer wants to host their own unofficial
+## private game.
+
+
+#region Variables
+var players := []
+#endregion
+
+## Sets up the server.
+func _ready():
+  initialize_server()
+  start_server()
+
+## Changes the multiplayer of this node to a new instance.
+func initialize_server():
+  pass
+
+## Starts the server and cleans up.
+func start_server():
+  pass
+
+## Stops the server and cleans up.
+func stop_server():
+  pass
+
+#
+# func reconnect_server():
+#   pass
+#
