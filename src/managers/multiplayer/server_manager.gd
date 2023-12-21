030b72518e6264ce77243d6bac5a6eb162dd2621
Makosai <kristopher.ali.dev@gmail.com>
2023-12-21 16:08:04 -0500

    Convert the ServerManager to a MultiplayerAPI instance.

--- src/managers/multiplayer/server_manager.gd
+++ src/managers/multiplayer/server_manager.gd
@@ -1,4 +1,4 @@
-class_name ServerManager extends Node
+class_name ServerManager extends MultiplayerAPI
 ## Creates a server and handles how many people can join it.
 ##
 ## The ServerManager is a part of the Multiplayer namespace.
