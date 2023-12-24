043af10ee66b04d1340dc35074e30464c8aa9b80
Makosai <kristopher.ali.dev@gmail.com>
2023-12-24 10:43:14 -0500

    Clean up documentation and code.

--- src/managers/multiplayer/client_manager.gd
+++ src/managers/multiplayer/client_manager.gd
@@ -17,7 +17,7 @@ var host = "127.0.0.1"
 ## Default port.
 const DEF_PORT = 4337
 
-## Our WebSocketServer instance.
+## Our WebSocketClient instance.
 var _client := WebSocketMultiplayerPeer.new()
 #endregion
 
@@ -27,8 +27,6 @@ var players := []
 ## Sets up the server.
 func _ready():
   setup_signals()
-
-  initialize_client()
   start_client()
 
 ## Sets up the client connected and disconnected signals.
@@ -37,13 +35,9 @@ func setup_signals():
   multiplayer.peer_disconnected.connect(_on_client_disconnected)
   multiplayer.server_disconnected.connect(_on_server_disconnected)
 
-
-## Cleans up the peer.
-func initialize_client():
-  multiplayer.multiplayer_peer = null
-
 ## Starts the server and updates the peer.
 func start_client():
+  multiplayer.multiplayer_peer = null
   _client.create_client("ws://" + host + ":" + str(DEF_PORT))
   multiplayer.multiplayer_peer = _client
   print("Server Started")
@@ -60,13 +54,10 @@ func stop_server():
 #region Signal Handlers
 func _on_client_connected(id: int):
   print("Client (%d) Connected" % id)
-  pass
 
 func _on_client_disconnected(id: int):
   print("Client (%d) Disconnected" % id)
-  pass
 
 func _on_server_disconnected():
   print("Server Connection Lost")
-  pass
 #endregion
