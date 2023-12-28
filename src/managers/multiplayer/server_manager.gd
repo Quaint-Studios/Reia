45ef9b4f7346e97d5346f1cb27c25abccdf547c0
Makosai <kristopher.ali.dev@gmail.com>
2023-12-28 17:37:59 -0500

    Create a separate MultiplayerAPI instance.

--- src/managers/multiplayer/server_manager.gd
+++ src/managers/multiplayer/server_manager.gd
@@ -32,18 +32,19 @@ func _ready():
 
 ## Sets up the client connected and disconnected signals.
 func setup_signals():
-	peer_connected.connect(_on_client_connected)
-	peer_disconnected.connect(_on_client_disconnected)
+	get_tree().set_multiplayer(MultiplayerAPI.create_default_interface(), get_path())
+	multiplayer.peer_connected.connect(_on_client_connected)
+	multiplayer.peer_disconnected.connect(_on_client_disconnected)
 
 ## Cleans up the peer.
 func initialize_server():
-	multiplayer_peer = null
+	multiplayer.multiplayer_peer = null
 
 ## Starts the server and updates the peer.
 func start_server():
 	_server.create_server(DEF_PORT)
-	multiplayer_peer = _server
 	print("Server Started")
+	multiplayer.multiplayer_peer = _server
 
 ## Stops the server and cleans up.
 func stop_server():
