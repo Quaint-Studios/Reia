316ec35b7de960eb0d7b19156b04979d38686e1d
Makosai <kristopher.ali.dev@gmail.com>
2023-12-30 16:37:47 -0500

    Create a setup shell script for fast deployment.
    Former-commit-id: 78c1508c9bc8ceb079f5b8c36b6a34755b4b25d0

---
+++ .scripts/linux/setup.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+
+# Inputs
+DEFAULT_VERSION="4.2.1-stable"
+VERSION=${1:-$DEFAULT_VERSION}
+
+# Checksums
+declare -rA checksums=(["4.2.1-stable"]="86311a9e75b7445eb4215312e604df1f69c72d4641af3e67049e30356ae37660")
+
+# Variables
+URL="https://github.com/godotengine/godot/releases/download"
+PREFIX="Godot_v"
+SUFFIX="_linux.x86_64"
+EXT=".zip"
+FILE=$PREFIX$VERSION$SUFFIX
+
+
+# Get Files
+# wget ${URL}/${VERSION}/${FILE}${EXT}
+
+# Generate Checksum for new version of Godot if you need to add it above.
+# sha256sum ./Godot_v${VERSION}_linux.x86_64.zip
+
+
+# Validate Checksum
+CHECKSUM256=${checksums[$VERSION]}
+echo $CHECKSUM256 $FILE$EXT  | sha256sum -c --quiet >/dev/null 2>&1
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+    echo -n "PASS "
+else
+    echo -n "FAIL "
+    exit 0
+fi
+
+# Unzip
+apt install unzip -y
+unzip ./$FILE$EXT -d ../../ # Extract to Reia's directory
+
+# Run Godot and build
+./$FILE --path ./project.godot --export "linux_server" .builds/server/Reia
+
+exit 1
