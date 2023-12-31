d7b324e20158fc339f2ecd16348533087f54ada7
Makosai <kristopher.ali.dev@gmail.com>
2023-12-31 18:55:34 -0500

    Automatically load scenes depending if the executable is a server or a player.
    Former-commit-id: 8f4962b3aee511833b188722fe5822aa0848d51c

--- src/scene_selector.gd
+++ src/scene_selector.gd
@@ -5,3 +5,9 @@ const MAIN_MENU = SCENE_FOLDER + "/main_menu/main_menu.tscn"
 const REIA = SCENE_FOLDER + "reia/reia.tscn"
 
 enum Maps { REIA }
+
+func _ready():
+	if OS.has_feature("dedicated_server") || DisplayServer.get_name() == "headless" ||  "--server" in OS.get_cmdline_user_args():
+		get_tree().change_scene_to_file(SceneSelector.REIA)
+	else:
+		get_tree().change_scene_to_file(SceneSelector.MAIN_MENU)
