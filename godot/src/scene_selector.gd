12ca9e562d624fd49572674064080fe2cf44f275
Makosai <kristopher.ali.dev@gmail.com>
2024-01-27 00:43:16 -0500

    Update the SceneSelector maps.
    Former-commit-id: e9cd6dfef272af2db13be35b35a2009f8c710f10

--- godot/src/scene_selector.gd
+++ godot/src/scene_selector.gd
@@ -1,13 +1,14 @@
 class_name SceneSelector extends Node
 
 const SCENE_FOLDER = "res://scenes/"
-const MAIN_MENU = SCENE_FOLDER + "/main_menu/main_menu.tscn"
-const REIA = SCENE_FOLDER + "reia/reia.tscn"
+const MAIN_MENU = SCENE_FOLDER + "maps/1_main_menu.tscn"
+const JADEWATER_FALLS = SCENE_FOLDER + "maps/2_jadewater_falls.tscn"
+const WATERBROOK = SCENE_FOLDER + "maps/3_waterbrook.tscn"
 
-enum Maps { REIA }
+enum Maps { JADEWATER_FALLS, WATERBROOK }
 
 func _ready():
 	if OS.has_feature("dedicated_server") || DisplayServer.get_name() == "headless" ||  "--server" in OS.get_cmdline_user_args():
-		get_tree().change_scene_to_file.call_deferred(SceneSelector.REIA)
+		get_tree().change_scene_to_file.call_deferred(SceneSelector.JADEWATER_FALLS)
 	else:
 		get_tree().change_scene_to_file.call_deferred(SceneSelector.MAIN_MENU)
