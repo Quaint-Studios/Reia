1c4aa49e98418bf4b2dff2706c29c9d3ef4401ec
Makosai <kristopher.ali.dev@gmail.com>
2024-04-08 18:32:45 -0400

    Reactivate DiscordSDK.
    Former-commit-id: 9036cc4c204d8cca600ac33446cf986c1c372303

--- godot/scenes/misc/scripts/managers/discord_manager.gd
+++ godot/scenes/misc/scripts/managers/discord_manager.gd
@@ -1,13 +1,14 @@
 extends Node
+## Discord SDK
+##
+## This script is used to set the Rich Presence of the Discord SDK.
 
 func _ready():
-	return
-"""
 	discord_sdk.app_id = 688581385639952418 # Application ID
 	discord_sdk.details = "Roaming the world..."
 	discord_sdk.state = "Playing Solo"
 
-	discord_sdk.large_image = "logo_reia" # Image key from "Art Assets"
+	discord_sdk.large_image = "logo_a_1024" # Image key from "Art Assets"
 	discord_sdk.large_image_text = ""
 	discord_sdk.small_image = "" # Image key from "Art Assets"
 	discord_sdk.small_image_text = ""
@@ -19,4 +20,3 @@ func _ready():
 	# discord_sdk.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
 
 	discord_sdk.refresh() # Always refresh after changing the values!
-"""
