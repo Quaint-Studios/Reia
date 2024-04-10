af7f27e3813cb769bcc92ac287aef79227055641
Makosai <kristopher.ali.dev@gmail.com>
2024-04-10 16:11:46 -0400

    Fix the PlayerInteractor and the UI to go with it.

--- godot/scenes/misc/scripts/managers/ui_manager/player_ui__interactor.gd
+++ godot/scenes/misc/scripts/managers/ui_manager/player_ui__interactor.gd
@@ -1,21 +1,4 @@
-class_name PlayerUI_Interactor extends RayCast3D
-## Handles the player's interaction with the world.
+class_name PlayerUI_Interactor extends Control
+## A container for the player's interactor UI elements.
 
-# Shows a UI element when the player is close to an interactable object.
-
-@export var max_interactable_range := 100.0
-
-func _process(_delta: float) -> void:
-	var coll := get_collider() as Interactable
-
-	if is_colliding():
-		print("is colliding")
-
-		if coll.is_in_group("interactable"):
-			print("is interactable")
-			if (owner as CharacterBody3D).position.distance_to(coll.position) <= max_interactable_range:
-				print("is in range")
-				UIManager.instance.hud.show_interactor(coll.interact_name)
-				return
-
-	UIManager.instance.player.hud.hide_interactor()
+@onready var object_name: Label = $Prompt/ObjectName
