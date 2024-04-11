ad28c41915527cb17366f4a7a8d955cc243a4177
Makosai <kristopher.ali.dev@gmail.com>
2024-04-10 22:37:26 -0400

    Improve the Main Menu scene to have interactable buttons.
    Former-commit-id: d0e969ee5846afa20ccd2192ec2e567c1b430bf7

---
+++ godot/scenes/maps/scripts/1_main_menu/main_menu_title.gd
@@ -0,0 +1,61 @@
+extends Control
+
+@onready var play: MarginContainer = $Play
+@onready var settings: MarginContainer = $Settings
+@onready var credits: MarginContainer = $Credits
+@onready var quit: MarginContainer = $Quit
+
+@onready var playButton: TextureButton = $Play/TextureButton
+@onready var settingsButton: TextureButton = $Settings/TextureButton
+@onready var creditsButton: TextureButton = $Credits/TextureButton
+@onready var quitButton: TextureButton = $Quit/TextureButton
+
+func _ready():
+		playButton.pressed.connect(_on_play_pressed)
+		settingsButton.pressed.connect(_on_settings_pressed)
+		creditsButton.pressed.connect(_on_credits_pressed)
+		quitButton.pressed.connect(_on_quit_pressed)
+
+		playButton.mouse_entered.connect(_on_mouse_entered.bind("Play", playButton))
+		settingsButton.mouse_entered.connect(_on_mouse_entered.bind("Settings", settingsButton))
+		creditsButton.mouse_entered.connect(_on_mouse_entered.bind("Credits", creditsButton))
+		quitButton.mouse_entered.connect(_on_mouse_entered.bind("Quit", quitButton))
+
+		playButton.mouse_exited.connect(_on_mouse_exited.bind("Play", playButton))
+		settingsButton.mouse_exited.connect(_on_mouse_exited.bind("Settings", settingsButton))
+		creditsButton.mouse_exited.connect(_on_mouse_exited.bind("Credits", creditsButton))
+		quitButton.mouse_exited.connect(_on_mouse_exited.bind("Quit", quitButton))
+
+# On mouse entered for, change the size of the adjacent Label
+func _on_mouse_entered(labelName: String, btn: TextureButton):
+	var label := btn.get_node("../HBoxContainer/%sLabel" % labelName) as Label
+
+	if label == null:
+		print("Label not found.")
+		return
+
+	label.set("theme_override_font_sizes/font_size", 40)
+	label.set("theme_override_font_colors/font_color", Color.from_string("#f5f5f5", Color(0.96, 0.96, 0.96, 1)))
+
+func _on_mouse_exited(labelName: String, btn: TextureButton):
+	var label := btn.get_node("../HBoxContainer/%sLabel" % labelName) as Label
+
+	if label == null:
+		print("Label not found.")
+		return
+
+	label.set("theme_override_font_sizes/font_size", 32)
+	label.set("theme_override_font_colors/font_color", Color.from_string("#f9f3e5", Color(0.98, 0.95, 0.9, 1)))
+
+func _on_play_pressed():
+	pass
+
+func _on_settings_pressed():
+	pass
+
+func _on_credits_pressed():
+	pass
+
+func _on_quit_pressed():
+	# TODO: Verify the quit
+	get_tree().quit()
