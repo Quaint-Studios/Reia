07285b866faa246fa9b1e59992ae898807db73b1
Makosai <kristopher.ali.dev@gmail.com>
2024-04-16 19:20:34 -0400

    Upgrade the Discord SDK addon.

--- godot/scenes/misc/scripts/managers/discord_manager.gd
+++ godot/scenes/misc/scripts/managers/discord_manager.gd
@@ -4,19 +4,19 @@ extends Node
 ## This script is used to set the Rich Presence of the Discord SDK.
 
 func _ready():
-	discord_sdk.app_id = 688581385639952418 # Application ID
-	discord_sdk.details = "Roaming the world..."
-	discord_sdk.state = "Playing Solo"
+	DiscordRPC.app_id = 688581385639952418 # Application ID
+	DiscordRPC.details = "Roaming the world..."
+	DiscordRPC.state = "Playing Solo"
 
-	discord_sdk.large_image = "logo_a_1024" # Image key from "Art Assets"
-	discord_sdk.large_image_text = ""
-	discord_sdk.small_image = "" # Image key from "Art Assets"
-	discord_sdk.small_image_text = ""
+	DiscordRPC.large_image = "logo_a_1024" # Image key from "Art Assets"
+	DiscordRPC.large_image_text = ""
+	DiscordRPC.small_image = "" # Image key from "Art Assets"
+	DiscordRPC.small_image_text = ""
 
-	discord_sdk.current_party_size = 1
-	discord_sdk.max_party_size = 5
+	DiscordRPC.current_party_size = 1
+	DiscordRPC.max_party_size = 5
 
-	discord_sdk.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
-	# discord_sdk.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
+	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+	# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
 
-	discord_sdk.refresh() # Always refresh after changing the values!
+	DiscordRPC.refresh() # Always refresh after changing the values!
