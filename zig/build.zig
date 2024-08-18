df3f85282ab7ff3e2482bcee1aea23bda1880cef
Makosai <kristopher.ali.dev@gmail.com>
2024-08-18 10:10:50 -0400

    Rename main to Zig and update build.zig.

--- zig/build.zig
+++ zig/build.zig
@@ -7,20 +7,31 @@ pub fn build(b: *std.Build) !void {
 
     const lib = b.addSharedLibrary(.{
         .name = "reia",
-        .root_source_file = .{ .path = "src/Reia.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const tests = b.addTest(.{
+        .name = "test",
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/main.zig"),
+        .test_runner = b.path("test_runner.zig"),
+    });
+
     const godot_zig_build = @import("./godot-zig/build.zig");
     const godot = godot_zig_build.createModule(b, target, optimize, godot_path);
+
     lib.root_module.addImport("godot", godot);
+    tests.root_module.addImport("godot", godot);
 
     // use explicit imports to make jump work properly
     // todo: remove this once zls get improved
     var iter = godot.import_table.iterator();
     while (iter.next()) |it| {
         lib.root_module.addImport(it.key_ptr.*, it.value_ptr.*);
+        tests.root_module.addImport(it.key_ptr.*, it.value_ptr.*);
     }
     /////////////////////////////////////////////////
 
@@ -30,7 +41,14 @@ pub fn build(b: *std.Build) !void {
     const run_cmd = b.addSystemCommand(&.{
         godot_path, "--path", "../godot",
     });
+
     run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run with Godot");
     run_step.dependOn(&run_cmd.step);
+
+    const run_tests = b.addRunArtifact(tests);
+    run_tests.has_side_effects = true;
+    run_tests.step.dependOn(b.getInstallStep());
+    const test_step = b.step("test", "Run tests with Godot");
+    test_step.dependOn(&run_tests.step);
 }
