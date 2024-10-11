a73429f4075e5e758fe01fe241ba41adc5aee2e0
Makosai <kristopher.ali.dev@gmail.com>
2024-10-11 00:27:58 -0400

    Swap to asserting signal connect values instead.

--- godot/scenes/menus/main_menu/main_menu_title.gd
+++ godot/scenes/menus/main_menu/main_menu_title.gd
@@ -11,45 +11,29 @@ extends Control
 @onready var quitButton: TextureButton = $Quit/TextureButton
 
 func _ready() -> void:
-	if !playButton.pressed.is_connected(_on_play_pressed):
-		var __ := playButton.pressed.connect(_on_play_pressed)
+		assert(playButton.pressed.connect(_on_play_pressed) == 0)
+		assert(settingsButton.pressed.connect(_on_settings_pressed) == 0)
+		assert(creditsButton.pressed.connect(_on_credits_pressed) == 0)
+		assert(quitButton.pressed.connect(_on_quit_pressed) == 0)
 
-	if !settingsButton.pressed.is_connected(_on_settings_pressed):
-		var __ := settingsButton.pressed.connect(_on_settings_pressed)
+		assert(playButton.mouse_entered.connect(_on_mouse_entered.bind("Play", playButton)) == 0)
+		assert(settingsButton.mouse_entered.connect(_on_mouse_entered.bind("Settings", settingsButton)) == 0)
+		assert(creditsButton.mouse_entered.connect(_on_mouse_entered.bind("Credits", creditsButton)) == 0)
+		assert(quitButton.mouse_entered.connect(_on_mouse_entered.bind("Quit", quitButton)) == 0)
 
-	if !creditsButton.pressed.is_connected(_on_credits_pressed):
-		var __ := creditsButton.pressed.connect(_on_credits_pressed)
-
-	if !quitButton.pressed.is_connected(_on_quit_pressed):
-		var __ := quitButton.pressed.connect(_on_quit_pressed)
-
-
-	if !playButton.mouse_entered.is_connected(_on_mouse_entered.bind("Play", playButton)):
-		var __ := playButton.mouse_entered.connect(_on_mouse_entered.bind("Play", playButton))
-	if !settingsButton.mouse_entered.is_connected(_on_mouse_entered.bind("Settings", settingsButton)):
-		var __ := settingsButton.mouse_entered.connect(_on_mouse_entered.bind("Settings", settingsButton))
-	if !creditsButton.mouse_entered.is_connected(_on_mouse_entered.bind("Credits", creditsButton)):
-		var __ := creditsButton.mouse_entered.connect(_on_mouse_entered.bind("Credits", creditsButton))
-	if !quitButton.mouse_entered.is_connected(_on_mouse_entered.bind("Quit", quitButton)):
-		var __ := quitButton.mouse_entered.connect(_on_mouse_entered.bind("Quit", quitButton))
-
-	if !playButton.mouse_exited.is_connected(_on_mouse_exited.bind("Play", playButton)):
-		var __ := playButton.mouse_exited.connect(_on_mouse_exited.bind("Play", playButton))
-	if !settingsButton.mouse_exited.is_connected(_on_mouse_exited.bind("Settings", settingsButton)):
-		var __ := settingsButton.mouse_exited.connect(_on_mouse_exited.bind("Settings", settingsButton))
-	if !creditsButton.mouse_exited.is_connected(_on_mouse_exited.bind("Credits", creditsButton)):
-		var __ := creditsButton.mouse_exited.connect(_on_mouse_exited.bind("Credits", creditsButton))
-	if !quitButton.mouse_exited.is_connected(_on_mouse_exited.bind("Quit", quitButton)):
-		var __ := quitButton.mouse_exited.connect(_on_mouse_exited.bind("Quit", quitButton))
+		assert(playButton.mouse_exited.connect(_on_mouse_exited.bind("Play", playButton)) == 0)
+		assert(settingsButton.mouse_exited.connect(_on_mouse_exited.bind("Settings", settingsButton)) == 0)
+		assert(creditsButton.mouse_exited.connect(_on_mouse_exited.bind("Credits", creditsButton)) == 0)
+		assert(quitButton.mouse_exited.connect(_on_mouse_exited.bind("Quit", quitButton)) == 0)
 
 # On mouse entered for, change the size of the adjacent Label
 func _on_mouse_entered(labelName: String, btn: TextureButton) -> void:
 	var label := btn.get_node("../HBoxContainer/%sLabel" % labelName) as Label
-	var decorations : Array[Control] = [
+	var decorations: Array[Control] = [
 		btn.get_node("../HBoxContainer/EllipseL") as Control,
 		btn.get_node("../HBoxContainer/StarL") as Control,
-	 	btn.get_node("../HBoxContainer/EllipseR") as Control,
-	 	btn.get_node("../HBoxContainer/StarR") as Control
+		btn.get_node("../HBoxContainer/EllipseR") as Control,
+		btn.get_node("../HBoxContainer/StarR") as Control
 	]
 
 	if decorations.has(null):
@@ -69,11 +53,11 @@ func _on_mouse_entered(labelName: String, btn: TextureButton) -> void:
 func _on_mouse_exited(labelName: String, btn: TextureButton) -> void:
 	var label := btn.get_node("../HBoxContainer/%sLabel" % labelName) as Label
 
-	var decorations : Array[Control] = [
+	var decorations: Array[Control] = [
 		btn.get_node("../HBoxContainer/EllipseL") as Control,
 		btn.get_node("../HBoxContainer/StarL") as Control,
-	 	btn.get_node("../HBoxContainer/EllipseR") as Control,
-	 	btn.get_node("../HBoxContainer/StarR") as Control
+		btn.get_node("../HBoxContainer/EllipseR") as Control,
+		btn.get_node("../HBoxContainer/StarR") as Control
 	]
 
 	if decorations.has(null):
