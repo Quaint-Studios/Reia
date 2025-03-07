ebdfb1fa362785e8ae2872b87d142dd8226efbc9
Makosai <kristopher.ali.dev@gmail.com>
2025-03-07 18:21:15 -0500

    Remove the todo folder.
    Former-commit-id: fe66263f68c509c8ff12310a70f6b02af4d6a550

--- todo/scripts/items/data/soulstone_slot.gd
+++
@@ -1,7 +0,0 @@
-class_name SoulstoneSlot extends Resource
-
-@export var slot_type : Enums.Soulstone_Type
-@export var slot_affinity : Enums.Element_Affinity
-
-func add_soulstone(soulstone: SoulstoneData):
-	pass
--- todo/scripts/items/data/types/equipment_data.gd
+++
@@ -1,2 +0,0 @@
-class_name EquipmentData extends ItemData
-
