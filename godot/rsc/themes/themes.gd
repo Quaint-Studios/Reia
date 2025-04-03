2c985a86b1abeaba4505315e8b7a22354c22dca1
Makosai <kristopher.ali.dev@gmail.com>
2025-04-02 21:57:43 -0400

    Import all potential fonts and colors from the Figma design.
    Former-commit-id: 7cf442368bf5367b8ce40320c12771ca93a7678d

--- godot/rsc/themes/themes.gd
+++ godot/rsc/themes/themes.gd
@@ -3,18 +3,41 @@ extends ProgrammaticTheme
 
 class Colors:
 	class White:
-		const Pure = Color8(255, 255, 255)
-		const Creamy = Color8(249, 243, 229)
-		const Soft = Color8(245, 245, 245)
+		const PURE = Color8(255, 255, 255)
+		const CREAMY = Color8(249, 243, 229)
+		const SOFT = Color8(245, 245, 245)
 	class Black:
-		const Transparent = Color8(26, 26, 26, 51)
+		const TRANSPARENT = Color8(26, 26, 26, 51)
 	class Blue:
-		const Clear = Color8(95, 144, 151)
-		const Chip = Color8(85, 142, 221)
+		const CLEAR = Color8(95, 144, 151)
+		const CHIP = Color8(85, 142, 221)
+	class Red:
+		const CANCEL = Color8(249, 98, 47)
+	class Inventory:
+		const BAR = Color8(56, 90, 166, 165.75) # 65% alpha
+		const CONTENT = Color8(255, 255, 255, 25.5) # 10% alpha
+		class Item:
+			const COMMON = Color8(85, 221, 99)
+			const UNCOMMON = Color8(85, 126, 221)
+			const RARE = Color8(247, 237, 186)
+			const SACRED = Color8(186, 84, 248)
+			const ARCANE_1 = Color8(248, 87, 32)
+			const ARCANE_2 = Color8(250, 128, 87)
+			const RADIANT_1 = Color8(240, 211, 66)
+			const RADIANT_2 = Color8(255, 255, 255)
+			const TEXT_BG = Color8(52, 51, 63)
+			const TEXT_FG = Color8(250, 253, 255)
+			const HEADER_BG = Color8(52, 51, 63, 127.5) # 50% alpha
+			const HEADER_FG = Color8(250, 253, 255)
 
-#var font_normal := ResourceLoader.load("res://rsc/fonts/Roboto/Roboto-Regular.ttf") as FontFile
-#var font_bold := ResourceLoader.load("res://rsc/fonts/Roboto/Roboto-Bold.ttf") as FontFile
-const default_font_size := 20
+class Fonts:
+	class Size:
+		const NORMAL = 20
+	class Poppins:
+		var normal := ResourceLoader.load("res://rsc/fonts/poppins-latin-400-normal.ttf") as FontFile
+		var bold := ResourceLoader.load("res://rsc/fonts/poppins-latin-700-normal.ttf") as FontFile
+	class Roboto:
+		var black := ResourceLoader.load("res://rsc/fonts/roboto-latin-900-normal.ttf") as FontFile
 
 func setup() -> void:
 	set_save_path("res://rsc/themes/theme.tres")
@@ -30,7 +53,7 @@ func define_fonts() -> void:
 
 func define_font_default() -> void:
 	#define_default_font(font_normal)
-	define_default_font_size(default_font_size)
+	define_default_font_size(Fonts.Size.NORMAL)
 #endregion
 
 #region Labels
@@ -40,19 +63,19 @@ func define_labels() -> void:
 
 func define_label_default() -> void:
 	define_style("Label", {
-		font_color = Colors.White.Soft
+		font_color = Colors.White.SOFT
 	})
 
 func define_mainmenulabel_label() -> void:
 	define_variant_style("MainMenuLabel", "Label", {
 		#font = font_bold,
-		font_color = Colors.White.Creamy,
+		font_color = Colors.White.CREAMY,
 		font_size = 46,
 	})
 
 	define_variant_style("MainMenuLabelHovered", "Label", {
 		#font = font_bold,
-		font_color = Colors.White.Pure,
+		font_color = Colors.White.PURE,
 		font_size = 54
 	})
 #endregion
@@ -63,6 +86,6 @@ func define_buttons() -> void:
 
 func define_button_default() -> void:
 	define_style("Button", {
-		font_color = Colors.White.Soft
+		font_color = Colors.White.SOFT
 	})
 #endregion
