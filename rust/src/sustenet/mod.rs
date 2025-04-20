9a3e0969dd98fd79001a1dd9e03450e8beac90be
Makosai <kristopher.ali.dev@gmail.com>
2025-04-20 06:35:57 -0400

    Improve the Sustenet implementation and use crates.io.

--- rust/src/sustenet/mod.rs
+++ rust/src/sustenet/mod.rs
@@ -1,15 +1,25 @@
+// This file is current structured in a way to quickly test the Sustenet library.
+// It is not the final structure of the library.
+
+// The next steps will be cleaning up the code with macros and refining the approach
+// to make things easier to use and prettier.
+
+// For now, the master, cluster, and client all run perfectly. And it generates and loads
+// the config file correctly as well as the AES keys.
+
 use std::{
     sync::{Arc, Mutex},
     thread::sleep,
     time::Duration,
 };
 
-use client::cleanup;
-use cluster::{error, start};
 use godot::{global::godot_print, prelude::*};
 use godot_tokio::AsyncRuntime;
-use shared::Plugin;
+
 use tokio::sync::mpsc::Sender;
+
+use sustenet::shared::Plugin;
+use sustenet::{client, cluster, master};
 
 struct Reia {
     logs: Arc<Mutex<Vec<String>>>,
@@ -20,10 +30,16 @@ impl Plugin for Reia {
         tx: Sender<Box<[u8]>>,
         command: u8,
     ) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>> {
+        let logs = self.logs.clone();
         Box::pin(async move {
-            println!("Reia plugin handling command: {}", command);
+            if let Ok(mut logs) = logs.lock() {
+                logs.push(format!("Received External Command: {}", command));
+            }
             if let Err(e) = tx.send(Box::new([20])).await {
-                error(format!("Failed to send message. {e}").as_str());
+                if let Ok(mut logs) = logs.lock() {
+                    logs.push(format!("Failed to send message. {e}"));
+                }
+                cluster::error(format!("Failed to send message. {e}").as_str());
             }
         })
     }
@@ -45,34 +61,69 @@ impl SustenetCluster {
         let reia = Reia {
             logs: Arc::new(Mutex::new(vec![])),
         };
+
         let logs = reia.logs.clone();
-        let logs2 = Arc::new(Mutex::new(String::from("logs2")));
-        let logs2_clone = logs2.clone();
-        godot_print!("Starting the cluster server...");
-        AsyncRuntime::spawn(async move {
-            panic!();
-            start(reia).await;
-            cleanup().await;
+        let master_logs = reia.logs.clone();
+        let cluster_logs = reia.logs.clone();
+        let client_logs = reia.logs.clone();
+        
+        let master_thread = AsyncRuntime::spawn(async move {
+            master::start().await;
             {
-                let mut logs2_guard = logs2_clone.lock().unwrap();
-                *logs2_guard = String::from("fart");
+                let mut logs_guard = master_logs.lock().unwrap();
+                logs_guard.push("Master server started.".to_string());
+            }
+            sleep(Duration::from_secs(1));
+        });
+        godot_print!("The master has been started!");
+
+        let cluster_thread = AsyncRuntime::spawn(async move {
+            cluster::start(reia).await;
+            cluster::cleanup().await;
+            {
+                let mut logs_guard = cluster_logs.lock().unwrap();
+                logs_guard.push("Cluster server started.".to_string());
             }
             sleep(Duration::from_secs(1));
         });
         godot_print!("The cluster has been started!");
 
+        
+        let client_thread = AsyncRuntime::spawn(async move {
+            client::start(/* reia */).await;
+            {
+                let mut logs_guard = client_logs.lock().unwrap();
+                logs_guard.push("Client server started.".to_string());
+            }
+            sleep(Duration::from_secs(1));
+        });
+        godot_print!("The client has been started!");
+
         // Debug
         sleep(Duration::from_secs(3));
+
+        if master_thread.is_finished() {
+            godot_print!("Master thread finished.");
+        } else {
+            godot_print!("Master thread is still running.");
+        }
+        if cluster_thread.is_finished() {
+            godot_print!("Cluster thread finished.");
+        } else {
+            godot_print!("Cluster thread is still running.");
+        }
+        if client_thread.is_finished() {
+            godot_print!("Client thread finished.");
+        } else {
+            godot_print!("Client thread is still running.");
+        }
+
         godot_print!("Printing logs.");
 
         if let Ok(logsvec) = logs.lock() {
             for log in logsvec.iter() {
                 godot_print!("{log}");
             }
-        }
-
-        if let Ok(logs2_guard) = logs2.lock() {
-            godot_print!("{logs2_guard}");
         }
     }
 }
