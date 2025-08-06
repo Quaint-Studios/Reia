6459a879d4a167036d6f16028d0b1c9215ef5655
Makosai <kristopher.ali.dev@gmail.com>
2025-08-06 14:32:32 -0400

    Create a logger utility script.

---
+++ godot/tools/utils/logger.gd
@@ -0,0 +1,22 @@
+@tool
+extends Object
+class_name Logger
+
+var path: String
+var name: String
+
+func _init(_path: String, _name: String) -> void:
+	path = _path
+	name = _name
+
+## Log with a timestamp and the configured name prefix.
+func log(message: String) -> void:
+	var log_file: FileAccess = FileAccess.open(path, FileAccess.WRITE_READ)
+	if log_file:
+		log_file.seek_end()
+		var success: bool = log_file.store_line("%s [%s]: %s" % [Time.get_datetime_string_from_system(), name, message])
+		if not success:
+			push_error("Failed to write to log file: %s" % path)
+		log_file.close()
+	else:
+		push_error("Failed to open log file for writing: %s" % path)
