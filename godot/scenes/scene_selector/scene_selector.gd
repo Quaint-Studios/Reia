70aa4450c7be9698784f2e29766255dbf642657f
Makosai <kristopher.ali.dev@gmail.com>
2025-08-09 19:27:39 -0400

    Fix the scene selector.

--- godot/scenes/scene_selector/scene_selector.gd
+++ godot/scenes/scene_selector/scene_selector.gd
@@ -9,12 +9,12 @@ const SCENE_FOLDER = "res://scenes/"
 class CITIES:
 	const FOLDER = SCENE_FOLDER + "cities/"
 
-	const WATERBROOK = FOLDER + "waterbrook/waterbrook.tscn"
 
 class REGIONS:
 	const FOLDER = SCENE_FOLDER + "regions/"
 
 	const JADEWATER_FALLS = FOLDER + "jadewater_falls/jadewater_falls.tscn"
+	const WATERBROOK_CITY = FOLDER + "waterbrook_city/waterbrook_city.tscn"
 
 class DUNGEONS:
 	const DUNGEONS_FOLDER = SCENE_FOLDER + "dungeons/"
@@ -24,15 +24,15 @@ class DUNGEONS:
 		const AVAHS_GROVE = FOLDER + "avahs_grove.tscn"
 
 class MAIN:
-	const _FOLDER = SCENE_FOLDER + "main/"
+	const _FOLDER = SCENE_FOLDER
 
 	const TITLE_SCREEN = _FOLDER + "title_screen/title_screen.tscn"
 	const SCENE_SELECTOR = _FOLDER + "scene_selector/scene_selector.tscn"
 
-enum Maps { JADEWATER_FALLS, WATERBROOK }
+enum Maps {JADEWATER_FALLS, WATERBROOK_CITY}
 
 func _ready() -> void:
-	if OS.has_feature("dedicated_server") || DisplayServer.get_name() == "headless" ||  "--server" in OS.get_cmdline_user_args():
+	if OS.has_feature("dedicated_server") || DisplayServer.get_name() == "headless" || "--server" in OS.get_cmdline_user_args():
 		get_tree().change_scene_to_file.call_deferred(SceneSelector.REGIONS.JADEWATER_FALLS)
 	else:
 		get_tree().change_scene_to_file.call_deferred(SceneSelector.MAIN.TITLE_SCREEN)
