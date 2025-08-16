9e3d562a8b3f2fc46c2d28610421c9ddb8dbfae8
Makosai <kristopher.ali.dev@gmail.com>
2025-08-15 21:43:59 -0400

    Fix and test Godot Bevy.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,45 +1,15 @@
-use godot::classes::Engine;
 use godot::prelude::*;
-use godot_tokio::AsyncRuntime;
+
+use bevy::prelude::*;
+use godot_bevy::prelude::*;
 
 mod sustenet;
 
-struct Reia;
-#[gdextension]
-unsafe impl ExtensionLibrary for Reia {
-    fn on_level_init(level: InitLevel) {
-        match level {
-            InitLevel::Core => {}
-            InitLevel::Servers => {}
-            InitLevel::Scene => {
-                let mut engine = Engine::singleton();
+#[bevy_app]
+fn build_app(app: &mut App) {
+    app.add_systems(Startup, hello_world);
+}
 
-                // This is where we register our async runtime singleton.
-                engine.register_singleton(AsyncRuntime::SINGLETON, &AsyncRuntime::new_alloc());
-            }
-            InitLevel::Editor => {}
-        }
-    }
-
-    fn on_level_deinit(level: InitLevel) {
-        match level {
-            InitLevel::Core => {}
-            InitLevel::Servers => {}
-            InitLevel::Scene => {
-                let mut engine = Engine::singleton();
-
-                // Here is where we free our async runtime singleton from memory.
-                if let Some(async_singleton) = engine.get_singleton(AsyncRuntime::SINGLETON) {
-                    engine.unregister_singleton(AsyncRuntime::SINGLETON);
-                    async_singleton.free();
-                } else {
-                    godot_warn!(
-                        "Failed to find & free singleton -> {}",
-                        AsyncRuntime::SINGLETON
-                    );
-                }
-            }
-            InitLevel::Editor => {}
-        }
-    }
+fn hello_world() {
+    godot::prelude::godot_print!("Hello from godot-bevy!");
 }
