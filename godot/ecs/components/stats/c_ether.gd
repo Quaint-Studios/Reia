56eec123e0e19e138fd3c1a2ed7a17ca8c2790af
Makosai <kristopher.ali.dev@gmail.com>
2025-09-27 02:35:11 -0400

    Add return types to ether and void components.

--- godot/ecs/components/stats/c_ether.gd
+++ godot/ecs/components/stats/c_ether.gd
@@ -4,6 +4,6 @@ extends Component
 @export var current: float = 75.0
 @export var maximum: float = 75.0
 
-func _init(max_ether: float = 75.0):
+func _init(max_ether: float = 75.0) -> void:
 	maximum = max_ether
 	current = max_ether
--- godot/ecs/components/stats/c_health.gd
+++ godot/ecs/components/stats/c_health.gd
@@ -4,6 +4,6 @@ extends Component
 @export var current: float = 150.0
 @export var maximum: float = 150.0
 
-func _init(max_health: float = 150.0):
+func _init(max_health: float = 150.0) -> void:
 	maximum = max_health
 	current = max_health
