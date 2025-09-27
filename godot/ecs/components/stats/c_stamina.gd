147cb660cda85af5022714cfcfab5710610d52ac
Makosai <kristopher.ali.dev@gmail.com>
2025-09-27 02:38:05 -0400

    Add a return time for the stamina component.

--- godot/ecs/components/stats/c_stamina.gd
+++ godot/ecs/components/stats/c_stamina.gd
@@ -4,6 +4,6 @@ extends Component
 @export var current: float = 100.0
 @export var maximum: float = 100.0
 
-func _init(max_ether: float = 100.0):
+func _init(max_ether: float = 100.0) -> void:
 	maximum = max_ether
 	current = max_ether
