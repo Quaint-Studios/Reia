b21c90a659953efba501b827839bdf65ee7647b5
Makosai <kristopher.ali.dev@gmail.com>
2025-09-28 03:16:06 -0400

    Ignore the warning in ecs utils.

--- godot/ecs/ecs_utils.gd
+++ godot/ecs/ecs_utils.gd
@@ -3,9 +3,11 @@ class_name ECSUtils
 static func sync_transform(entity: Entity) -> void:
 	if entity.has_component(C_Transform):
 		var transform_comp: C_Transform = entity.get_component(C_Transform)
+		@warning_ignore("unsafe_property_access")
 		transform_comp.transform = entity.global_transform
 
 static func sync_component_to_transform(entity: Entity) -> void:
 	if entity.has_component(C_Transform):
 		var transform_comp: C_Transform = entity.get_component(C_Transform)
+		@warning_ignore("unsafe_property_access")
 		entity.global_transform = transform_comp.transform
