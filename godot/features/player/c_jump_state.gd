ca53329c45a85c1ed03f56873d27ee5c6507bbae
Makosai <kristopher.ali.dev@gmail.com>
2025-09-28 20:35:34 -0400

    Update the time to be less noticeable.

--- godot/features/player/c_jump_state.gd
+++ godot/features/player/c_jump_state.gd
@@ -1,7 +1,7 @@
 class_name C_JumpState
 extends Component
 
-const COYOTE_TIME: float = 0.25 # Seconds of coyote time
+const COYOTE_TIME: float = 0.1 # Seconds of coyote time
 const JUMP_BUFFER_TIME: float = 0.15
 
 var coyote_timer: float = 0.0
