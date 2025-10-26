771d37aa59aa84ddd6eb42dc73852ffdabffe2ce
Makosai <kristopher.ali.dev@gmail.com>
2025-10-26 09:38:22 -0400

    Modify the offset and min/max for the camera.

--- godot/features/camera/c_camera_state.gd
+++ godot/features/camera/c_camera_state.gd
@@ -7,9 +7,9 @@ extends Component
 
 @export var zoom_step: float = 0.5
 
-@export var min_pitch: float = -0.4
-@export var max_pitch: float = 1.2
+@export var min_pitch: float = -0.2
+@export var max_pitch: float = 1.4
 
-@export var look_point_offset: Vector3 = Vector3(0, 1.5, 0)
+@export var look_point_offset: Vector3 = Vector3(0, 1.25, 0)
 
 @export var mouse_sensitivity: float = 0.01
