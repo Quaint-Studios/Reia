2c179d008a04c5a1643b7ab7d50968e0a1d5242a
Makosai <kristopher.ali.dev@gmail.com>
2025-10-30 22:34:12 -0400

    Create the skeleton for a global camera class.

---
+++ godot/features/camera/g_camera.gd
@@ -0,0 +1,11 @@
+class_name CameraGlobal
+
+# Cache player, camera, and all necessary components for easy access
+
+func update() -> void:
+	# Do a single function call to handle all camera functionality
+	pass
+
+# handle input here
+
+# break down into smaller functions as much as possible to keep things organized
