20c5e66229ccabd2b0263e4433ed7822ba19ca7d
Makosai <kristopher.ali.dev@gmail.com>
2025-11-01 17:36:30 -0400

    Increase player run speed to 7.5
    Updated the run_speed variable in C_PlayerMovementConfig from 6.5 to 7.5 to allow the player character to move faster when running.

--- godot/features/player/c_player_movement_config.gd
+++ godot/features/player/c_player_movement_config.gd
@@ -2,5 +2,5 @@ class_name C_PlayerMovementConfig
 extends Component
 
 @export var walk_speed: float = 4.5
-@export var run_speed: float = 6.5
+@export var run_speed: float = 7.5
 @export var turn_rate: float = 18.0 # how fast the character faces move direction
