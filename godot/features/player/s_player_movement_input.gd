f513a44c2c0481560bf6d4a7acd02100e9566f5b
Makosai <kristopher.ali.dev@gmail.com>
2025-11-01 20:57:22 -0400

    Refactor player movement input processing
    Simplifies the process function by removing the loop and directly updating the first C_MoveInput component. Adds support for jump hold detection and ensures jump_pressed is set if already pressed.

--- godot/features/player/s_player_movement_input.gd
+++ godot/features/player/s_player_movement_input.gd
@@ -4,14 +4,13 @@ extends System
 func query() -> QueryBuilder:
 	return q.with_all([C_LocalPlayer, C_MoveInput]).iterate([C_MoveInput])
 
-func process(entities: Array[Entity], _components: Array[Array], _delta: float) -> void:
-	var move_inputs := _components[0] as Array
+func process(_entities: Array[Entity], components: Array[Array], _delta: float) -> void:
+	var move: C_MoveInput = components[0][0]
 
-	for i in range(entities.size()):
-		var move: C_MoveInput = move_inputs[i]
-		var x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-		var y := Input.get_action_strength("move_forward") - Input.get_action_strength("move_back")
-		move.dir = Vector2(x, y)
+	var x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y := Input.get_action_strength("move_forward") - Input.get_action_strength("move_back")
+	move.dir = Vector2(x, y)
 
-		move.sprint = Input.is_action_pressed("sprint") # e.g., Shift
-		move.jump_pressed = Input.is_action_just_pressed("jump")
+	move.sprint = Input.is_action_pressed("sprint") # e.g., Shift
+	move.jump_pressed = Input.is_action_just_pressed("jump") or move.jump_pressed
+	move.jump_held = Input.is_action_pressed("jump")
