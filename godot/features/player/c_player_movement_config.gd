a98664fdcaca6cd44fd00d6c1b1c3ad137572fa3
Makosai <kristopher.ali.dev@gmail.com>
2025-11-01 20:56:50 -0400

    Add jump and fall parameters to movement config
    Introduced jump_speed, jump_cut_multiplier, and fall_multiplier variables to C_PlayerMovementConfig for enhanced control over player movement. Also adjusted run_speed from 7.5 to 7.0.

--- godot/features/player/c_player_movement_config.gd
+++ godot/features/player/c_player_movement_config.gd
@@ -2,5 +2,10 @@ class_name C_PlayerMovementConfig
 extends Component
 
 @export var walk_speed: float = 4.5
-@export var run_speed: float = 7.5
+@export var run_speed: float = 7.0
+
+@export var jump_speed: float = 5.5
+@export var jump_cut_multiplier: float = 3.0
+@export var fall_multiplier: float = 1.5
+
 @export var turn_rate: float = 18.0 # how fast the character faces move direction
