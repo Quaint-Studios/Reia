fcab6b4e9233fa53fd681e9569ad67966baeb3dc
Makosai <kristopher.ali.dev@gmail.com>
2025-12-13 20:15:33 -0500

    Simplify the camera significantly.
    This also reduces the chances of someone getting motion sickness.

--- godot/features/camera/g_camera.gd
+++ godot/features/camera/g_camera.gd
@@ -49,45 +49,18 @@ func process(_delta: float) -> void:
 		if target == null:
 			return
 
-func physics_process(delta: float) -> void:
 	if not LocalCache.is_valid() or root == null or not (target is CharacterBody3D):
 		return
 
-	var body := target as CharacterBody3D
-	var planar_velocity := body.get_real_velocity()
-	planar_velocity.y = 0.0
-
-	var rig_forward := (-yaw_pivot.global_transform.basis.z).normalized()
-	var rig_right := yaw_pivot.global_transform.basis.x.normalized()
-
-	var forward_speed := planar_velocity.dot(rig_forward)
-	var lateral_speed := planar_velocity.dot(rig_right)
-
-	var target_forward := log(1.0 + absf(forward_speed) * FORWARD_SPEED_SCALE / 10000.0)
-	target_forward *= signf(forward_speed)
-	target_forward = clampf(target_forward, -OFFSET_FORWARD_IN_CAP, OFFSET_FORWARD_OUT_CAP)
-
-	var target_lateral := log(1.0 + absf(lateral_speed) * LATERAL_SPEED_SCALE / 10000.0)
-	target_lateral *= signf(lateral_speed)
-	target_lateral = clampf(target_lateral, -OFFSET_LATERAL_CAP, OFFSET_LATERAL_CAP)
-
-	var forward_factor := clampf(FORWARD_SMOOTH * delta, 0.0, 1.0)
-	var lateral_factor := clampf(LATERAL_SMOOTH * delta, 0.0, 1.0)
-
-	current_forward_offset = lerpf(current_forward_offset, target_forward, forward_factor)
-	current_lateral_offset = lerpf(current_lateral_offset, target_lateral, lateral_factor)
-
-	var desired := target.global_transform.origin + camera_state.offset
-	desired -= rig_forward * current_forward_offset
-	desired -= rig_right * current_lateral_offset
-
-	var position_factor := clampf(camera_state.position_smooth * delta, 0.0, 1.0)
-	root.global_transform.origin = root.global_transform.origin.lerp(desired, position_factor)
+	root.global_transform.origin = target.global_transform.origin + camera_state.offset
 
 	yaw_pivot.rotation.y = deg_to_rad(camera_state.yaw)
 	pitch_pivot.rotation.x = deg_to_rad(camera_state.pitch)
 	spring_arm.spring_length = camera_state.distance
 	camera.fov = camera_state.fov
+
+func physics_process(_delta: float) -> void:
+	pass
 
 
 func load_player() -> void:
