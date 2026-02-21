ae483e528e0b6e42ef868da15c4afa61f1931e72
Makosai <kristopher.ali.dev@gmail.com>
2026-02-21 02:02:09 -0500

    Add enums for the upcoming inventory update.

--- godot/features/enums.gd
+++ godot/features/enums.gd
@@ -1,6 +1,13 @@
 class_name Enums
 
+# Combat & Gameplay
 enum Resource_Type {ETHER, STAMINA, HEALTH, SHIELD}
 enum Element_Affinity {HYDRO, PYRO, ELECTRO, AERO, TERRA, LUMA, UMBRA}
+
+# Gear
 enum Soulstone_Type {CORE, MAJOR, MINOR}
 enum Weapon_Type {MELEE, BOW, SPELL}
+
+# Inventory
+enum ItemCategory {WEAPON, EQUIPMENT, SOULSTONE, CONSUMABLE, MATERIAL, QUEST_ITEM}
+enum ItemGrade {COMMON, UNCOMMON, RARE, SACRED, ARCANE, RADIANT}
