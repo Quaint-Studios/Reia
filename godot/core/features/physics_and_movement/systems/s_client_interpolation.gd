2bda15d43a43e05c71a7dd2b65d18da440fb1d9d
Makosai <kristopher.ali.dev@gmail.com>
2026-03-24 23:55:25 -0400

    add in the movement sync component

--- godot/core/features/physics_and_movement/systems/s_client_interpolation.gd
+++ godot/core/features/physics_and_movement/systems/s_client_interpolation.gd
@@ -1,17 +1,17 @@
 class_name ClientInterpolationSystem extends System
 
-func query():
-	return q.with_all([C_Transform, C_NetworkSync])
+func query() -> QueryBuilder:
+	return q.with_all([C_Transform, C_MovementSync])
 
 func process(entities: Array[Entity], _components: Array, delta: float) -> void:
 	for entity in entities:
-		var sync := entity.get_component(C_NetworkSync) as C_NetworkSync
+		var sync := entity.get_component(C_MovementSync) as C_MovementSync
 		var target_pos := sync.server_transform.origin
 		
 		if entity.has_component(C_LocalPlayer):
 			# Rubberband correction if prediction fails heavily
-			if entity.global_transform.origin.distance_to(target_pos) > 2.0:
-				entity.global_transform.origin = target_pos
+			if (entity as Node as Node3D).global_transform.origin.distance_to(target_pos) > 2.0:
+				(entity as Node as Node3D).global_transform.origin = target_pos
 		else:
 			# Smoothly interpolate other players/monsters visually
-			entity.global_transform.origin = entity.global_transform.origin.lerp(target_pos, delta * 15.0)
+			(entity as Node as Node3D).global_transform.origin = (entity as Node as Node3D).global_transform.origin.lerp(target_pos, delta * 15.0)
