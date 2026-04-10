df7963de8434c74e677d38cb6c1712eafd21bfd9
Makosai <kristopher.ali.dev@gmail.com>
2026-04-09 21:25:29 -0400

    improve the zone component to be more type safe

--- godot/core/features/spawning_and_zones/components/c_current_zone.gd
+++ godot/core/features/spawning_and_zones/components/c_current_zone.gd
@@ -1,7 +1,7 @@
 class_name C_CurrentZone extends Component
 
 ## Tracks the deterministic Zone ID the entity is currently residing in.
-@export var zone_id: int = 0
+@export var zone_id: Zone.ID = Zone.ID.WATERBROOK
 
-func _init(_zone_id: int = 0) -> void:
+func _init(_zone_id: Zone.ID = Zone.ID.WATERBROOK) -> void:
 	zone_id = _zone_id
